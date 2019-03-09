c3f686ab907b5bd2e77cccde7c217d1d2c0a4fbe
kelar86 <kelar86@mail.ru>
2019-03-09 20:49:22 +0700

    add searchresult log

--- monitor/models.py
+++ monitor/models.py
@@ -9,66 +9,66 @@ class AlertCategory(models.Model):
 
 class AlertStatus(models.Model):
     status_label = models.CharField(max_length=80)
-    status = models.CharField(max_length=80)
+    status = models.CharField(max_length=80, primary_key=True)
 
 
 class ProblemStatus(models.Model):
     status_label = models.CharField(max_length=80)
-    status = models.CharField(max_length=80)
+    status = models.CharField(max_length=80, primary_key=True)
 
 
 class Unit(models.Model):
     name = models.CharField(max_length=255)
-    icon = models.ImageField()
-    is_visiable = models.BooleanField()
+    icon = models.ImageField(null=True)
+    is_visiable = models.BooleanField(null=True)
 
 
 class Control(models.Model):
     name = models.CharField(max_length=255)
-    icon = models.ImageField()
-    is_visiable = models.BooleanField()
+    icon = models.ImageField(null=True)
+    is_visiable = models.BooleanField(null=True)
 
 
 class BodyType(models.Model):
     name = models.CharField(max_length=255)
-    icon = models.ImageField()
-    is_visiable = models.BooleanField()
+    icon = models.ImageField(null=True)
+    is_visiable = models.BooleanField(null=True)
 
 
 class Application(models.Model):
     name = models.CharField(max_length=255)
-    icon = models.ImageField()
-    has_controls = models.BooleanField()
-    is_visiable = models.BooleanField()
+    icon = models.ImageField(null=True)
+    has_controls = models.BooleanField(null=True)
+    is_visiable = models.BooleanField(null=True)
 
 
 class Alert(models.Model):
     alert_type = models.ForeignKey(
         AlertStatus, on_delete=models.SET_NULL, null=True)
     start_date = models.DateField()
-    finish_date = models.DateField()
+    finish_date = models.DateField(null=True)
     category = models.ForeignKey(AlertCategory, on_delete=models.CASCADE)
 
-    application = models.ForeignKey(Application, on_delete=models.CASCADE)
+    application = models.OneToOneField(Application, on_delete=models.CASCADE)
 
-    control = models.ManyToManyField(Control)
-    unit = models.ManyToManyField(Unit)
-    body_type = models.ManyToManyField(BodyType)
+    control = models.ManyToManyField(Control, blank=True)
+    unit = models.ManyToManyField(Unit, blank=True)
+    body_type = models.ManyToManyField(BodyType, blank=True)
 
-    description = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField(null=True)
+    author = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
 
 
 class Problem(models.Model):
     status = models.ForeignKey(
         ProblemStatus, on_delete=models.SET_NULL, null=True)
     detection_date = models.DateTimeField()
-    application = models.ForeignKey(Application, on_delete=models.CASCADE)
+    application = models.OneToOneField(Application, on_delete=models.CASCADE)
     alert = models.ForeignKey(Alert, on_delete=models.CASCADE, null=True)
 
-    control = models.ManyToManyField(Control)
-    unit = models.ManyToManyField(Unit)
-    body_type = models.ManyToManyField(BodyType)
+    control = models.ManyToManyField(Control, blank=True)
+    unit = models.ManyToManyField(Unit, blank=True)
+    body_type = models.ManyToManyField(BodyType, blank=True)
 
-    description = models.TextField()
-    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    description = models.TextField(null=True)
+    author = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
--- rest_api/models.py
+++ rest_api/models.py
@@ -1,9 +1,25 @@
+from monitor.models import *
 from django.db import models
 from django.contrib.auth.models import User
-from monitor.models import *
+from django.utils.timezone import now
 
 
-class QueryResult(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    application_result = models.ForeignKey(
-        Application, on_delete=models.SET_NULL, null=True)
+class SearchObject(models.Model):
+
+    application = models.OneToOneField(
+        Application, null=True, on_delete=models.CASCADE)
+
+    control = models.OneToOneField(
+        Control, null=True, on_delete=models.CASCADE)
+
+    unit = models.OneToOneField(
+        Unit, null=True, on_delete=models.CASCADE)
+
+    body_type = models.OneToOneField(
+        BodyType, null=True, on_delete=models.CASCADE)
+
+
+class SearchResult(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    date = models.DateTimeField(default=now, editable=False)
+    search_object = models.ForeignKey(SearchObject, on_delete=models.CASCADE)
