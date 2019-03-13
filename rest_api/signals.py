554d1ffbf9f83bf006a95f96655a4b9a750c0ee7
volynshchikov <volynshchikov@aigeo.ru>
2019-03-13 11:38:45 +0700

    translate models fields

--- rest_api/signals.py
+++ rest_api/signals.py
@@ -11,22 +11,22 @@ def update_search_advise(sender, **kwargs):
 
     last_problems = Problem.objects.order_by('-detection_date').all()[:10]
 
-    apps = [i + ('Application',)
-            for i in last_problems.values_list('application__id', 'application__name') if i[0]]
-    controls = [i + ('Control',)
-                for i in last_problems.values_list('control__id', 'control__name') if i[0]]
-    units = [i + ('Unit',)
-             for i in last_problems.values_list('unit__id', 'unit__name') if i[0]]
+    apps = [('Application',) + i
+            for i in last_problems.values_list('application__id', 'application__name', 'application__icon') if i[0]]
+    controls = [('Control',) + i
+                for i in last_problems.values_list('control__id', 'control__name', 'control__icon') if i[0]]
+    units = [('Unit',) + i
+             for i in last_problems.values_list('unit__id', 'unit__name', 'unit__icon') if i[0]]
 
-    body_types = [i + ('BodyType',)
-                  for i in last_problems.values_list('body_type__id', 'body_type__name') if i[0]]
+    body_types = [('BodyType',) + i
+                  for i in last_problems.values_list('body_type__id', 'body_type__name', 'body_type__icon') if i[0]]
 
     counted_list = Counter(list(chain(apps, controls, units, body_types)))
 
     result_list = sorted(counted_list.items(),
                          key=lambda item: item[1], reverse=True)
 
-    result_dict = [{'id': item[0][0], 'name': item[0][1], 'type': item[0][2]}
+    result_dict = [{'type': item[0][0], 'id': item[0][1], 'name': item[0][2], 'icon': item[0][3] }
                    for item in result_list]
 
     SearchAdvise.objects.update_or_create(
