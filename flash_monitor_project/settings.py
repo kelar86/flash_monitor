011e0d9c45289ce8c1eca5c48d147a634b8755fb
kelar86 <kelar86@mail.ru>
2019-03-14 20:38:14 +0700

    demo

--- flash_monitor_project/settings.py
+++ flash_monitor_project/settings.py
@@ -14,7 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 BASE_URL = 'http://localhost:8000'
 
@@ -130,10 +129,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, 'files', 'media')
 
 MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'files', 'media')
 
 
 REST_FRAMEWORK = {
